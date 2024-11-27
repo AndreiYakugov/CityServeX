6734afbd0a77d385ede549ad2b59576fa7df06e0
AndreiYakugov <tigerzero2@outlook.com>
2024-11-28 07:50:29 +0900

    updated landscape

--- src/app/components/Landscape.jsx
+++ src/app/components/Landscape.jsx
@@ -50,6 +50,7 @@ export default function Landscape() {
           width: 100%;
           height: 100%;
           object-fit: cover;
+          border-radius: 40px;
         }
         .indicator {
           position: absolute;
